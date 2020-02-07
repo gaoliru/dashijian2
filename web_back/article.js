c1f271ecae2fbbdca00d3d58ab00f021aff66655
gaoliru <406261232@qq.com>
2020-02-07 13:38:48 +0800

    首次提交

---
+++ web_back/article.js
@@ -0,0 +1,7 @@
+// 添加文章时的文件的本地预览功能
+// input标签type设为file
+// 把img图片的地址先指向本地的图片地址
+// 给文件域设事件 chenge事件
+// VAR SS =this.files[0] 保存着图片的名称
+// 把文件的地址设为当前标签的地址src
+// URL.createObjectURL(SS)浏览器临时创建的对象的地址
--- web_back/config.js
+++ web_back/config.js
@@ -1,6 +1,15 @@
 var BASEURL = "http://localhost:8000";
+// 接口基地址 
 var APILIST = {
     user_login: BASEURL + "/admin/login",
+
     user_logout: BASEURL + "/admin/logout",
-    user_getInfo: BASEURL + "admin/getuser"
+    user_getInfo: BASEURL + "admin/getuser",
+    // 获取文章分类
+    category_get: BASEURL + '/admin/category_search',
+    // 添加文章分类
+    category_add: BASEURL + '/admin/category_add',
+    // 删除文章分类
+    category_del: BASEURL + '/admin/category_delete',
+
 }
